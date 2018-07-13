b9429cb097849467c304c7fc3b3c21e04ee267b4
Pavel Macek <matcheck@matcheck.cz>
2018-07-13 09:14:27 +0200

    Time function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let seconds = 0, minutes = 0, hours = 0,t, time = "00:00";
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    time = (hours ? (hours > 9 ? hours : "0" + hours + ":") : "") + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer(time);
+}
+
+function timer(time) {
+    t = setTimeout(add, 1000);
+    console.log(time);
+    return time
+}
+
+// Timer
+const clock = (
+  <h1>00:00</h1>
+);
+
+// Cointainer component
 class App extends Component {
+  onButtonClick() {
+    timer();
+  }
   render() {
     return (
       <div className="App">
         <div className="Content">
-          <h1>00:00</h1>
-          <button type="button" className="Btn">Start</button>
+          {clock}
+          <button onClick={this.onButtonClick} type="button" className="Btn">Start</button>
         </div>
       </div>
     );
