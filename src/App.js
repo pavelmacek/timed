b4aeed7e46410113fc6197ba3615f4781d1ec1f8
Pavel Macek <matcheck@matcheck.cz>
2018-07-16 16:57:51 +0200

    Add start/pause functionality. Hurray, we have a functioning (although terribly coded) timer.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 
 var seconds = 0, minutes = 0, hours = 0,t, time = "00:00";
 
-
 // Cointainer component
 class App extends Component {
   constructor (props) {
     super(props);
-    this.state = { time: '00:00'};
+    this.state = { time: '00:00', running: false };
     this.add = this.add.bind(this);
     this.timer = this.timer.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
-  add() {
+  add() { // Instead of using this method and keep time in h m s, I should keep time in seconds and ++ after 1s, and always just calculate time to display with a separate secondsToTime function https://stackoverflow.com/questions/40885923/countdown-timer-in-react
       seconds++;
       if (seconds >= 60) {
           seconds = 0;
@@ -30,15 +30,23 @@ class App extends Component {
   }
   timer() {
       t = setTimeout(this.add, 1000);
-      this.setState({ time: time})
+      this.setState({ time: time, running: true})
       console.log(time);
+  }
+  onButtonClick() {
+    if (this.state.running) {
+      clearTimeout(t);
+      this.setState({ running: false})
+    } else {
+    this.timer();
+    }
   }
   render() {
     return (
       <div className="App">
         <div className="Content">
           <h1>{this.state.time}</h1>
-          <button onClick={this.timer} type="button" className="Btn">Start</button>
+          <button onClick={this.onButtonClick} type="button" className="Btn">{this.state.running ? 'Pause' : 'Start'}</button>
         </div>
       </div>
     );
