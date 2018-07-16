7d7c8688c8c0485164277b0b606219b614987ff6
Pavel Macek <matcheck@matcheck.cz>
2018-07-16 14:14:02 +0200

    Horrible code but the timer is working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-let seconds = 0, minutes = 0, hours = 0,t, time = "00:00";
+var seconds = 0, minutes = 0, hours = 0,t, time = "00:00";
 
-function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
-
-    time = (hours ? (hours > 9 ? hours : "0" + hours + ":") : "") + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-    timer(time);
-}
-
-function timer(time) {
-    t = setTimeout(add, 1000);
-    console.log(time);
-    return time
-}
-
-// Timer
-const clock = (
-  <h1>00:00</h1>
-);
 
 // Cointainer component
 class App extends Component {
-  onButtonClick() {
-    timer();
+  constructor (props) {
+    super(props);
+    this.state = { time: '00:00'};
+    this.add = this.add.bind(this);
+    this.timer = this.timer.bind(this);
+  }
+  add() {
+      seconds++;
+      if (seconds >= 60) {
+          seconds = 0;
+          minutes++;
+          if (minutes >= 60) {
+              minutes = 0;
+              hours++;
+          }
+      }
+
+      time = (hours ? (hours > 9 ? hours : "0" + hours + ":") : "") + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+      this.timer();
+  }
+  timer() {
+      t = setTimeout(this.add, 1000);
+      this.setState({ time: time})
+      console.log(time);
   }
   render() {
     return (
       <div className="App">
         <div className="Content">
-          {clock}
-          <button onClick={this.onButtonClick} type="button" className="Btn">Start</button>
+          <h1>{this.state.time}</h1>
+          <button onClick={this.timer} type="button" className="Btn">Start</button>
         </div>
       </div>
     );
