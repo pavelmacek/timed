192ecb78b50a112a617e4c031208b83bc1f56399
Pavel Macek <matcheck@matcheck.cz>
2018-07-19 09:18:00 +0200

    Add ability to create new timer

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ class App extends Component {
     this.state = { time: {hours: '00', minutes: '00', seconds: '00'}, running: false, seconds: 0, t: {}};
     this.add = this.add.bind(this);
     this.timer = this.timer.bind(this);
-    this.onButtonClick = this.onButtonClick.bind(this);
     this.secondsToTime = this.secondsToTime.bind(this);
+    this.controlTimer = this.controlTimer.bind(this);
+    this.newTimer = this.newTimer.bind(this);
   }
   add() {
       let seconds = this.state.seconds;
       seconds++;
       this.setState({seconds: seconds});
       console.log(seconds);
+
       this.setState({time: this.secondsToTime(seconds)});
       console.log(this.state.time);
+
       this.timer();
   }
   secondsToTime(secs){
@@ -42,7 +45,7 @@ class App extends Component {
       this.setState({ running: true})
 
   }
-  onButtonClick() {
+  controlTimer() {
     if (this.state.running) {
       clearTimeout(this.state.t);
       this.setState({ running: false})
@@ -50,12 +53,17 @@ class App extends Component {
     this.timer();
     }
   }
+  newTimer() {
+    this.setState({seconds: 0});
+    this.setState({time: {hours: '00', minutes: '00', seconds: '00'}});
+  }
   render() {
     return (
-      <div className="App">
-        <div className="Content">
+      <div className="app">
+        <div className="content">
           <h1>{this.state.time.hours == '00' ? '' : this.state.time.hours+':'}{this.state.time.minutes}:{this.state.time.seconds}</h1>
-          <button onClick={this.onButtonClick} type="button" className="Btn">{this.state.running ? 'Pause' : 'Start'}</button>
+          <button onClick={this.controlTimer} type="button" className="btn">{this.state.running ? 'Pause' : 'Start'}</button>
+          <button onClick={this.newTimer} type="button" className="btn btnSmall">New</button>
         </div>
       </div>
     );
